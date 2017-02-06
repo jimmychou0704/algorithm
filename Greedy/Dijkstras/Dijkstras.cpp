18844ead1aab6cfda7aafbe66302ec2191981654
JimmyChou <cchou9@gmail.com>
2017-02-06 00:12:59 +0000

    Dijkstras: set up  graph

--- Greedy/Dijkstras/Dijkstras.cpp
+++ Greedy/Dijkstras/Dijkstras.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <string>
+#include <sstream>
 #include "graph.h"
 #include "heap.h"
 
@@ -16,10 +18,34 @@ data.txt:  contains 200 nodes, start with the node number followed by many (endn
 
 int main(){
     
+    //read data
     ifstream data("data.txt");
     string line;
     
+    //construct a graph with 200 nodes
     graph g(200);
+    //updating the cost according to data.txt
+    while (getline(data, line)){
+        vector <int> curr_line;
+        
+        
+        stringstream ss(line);
+        int i;
+        while (ss >> i){
+                        if (ss.peek() == ',')
+                        ss.ignore();
+                        curr_line.push_back(i);
+                        
+        }
+        
+        
+        for (int i = 1; i< curr_line.size(); i += 2){
+            g.add_edge(curr_line[0], curr_line[i], curr_line[i+1]);
+        }
+        
+    }
+    
+    g.print(1);
     
     
     return 0;
