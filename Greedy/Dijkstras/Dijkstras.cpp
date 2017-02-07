b3dd4ea24704a7bdcb277311c4a9a3e06cf42d35
JimmyChou <cchou9@gmail.com>
2017-02-07 07:40:51 +0000

    dijkstras issue at the last node

--- Greedy/Dijkstras/Dijkstras.cpp
+++ Greedy/Dijkstras/Dijkstras.cpp
@@ -5,7 +5,7 @@
 #include <sstream>
 #include "graph.h"
 #include "heap.h"
-#include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
@@ -24,22 +24,63 @@ int dijkstra(const graph& g, int src, int end){
     long veryLargeNum = 100000;
     
     //array keeping track of min cost from src to all other nodes
-    int sol[g.size()];
+    int sol[g.size()+1];
     for (int i = 1; i<= g.size(); i++){
         sol[i] = i==src? 0: veryLargeNum;
     }
     
-    //first int: nodes been visited, second int: lin cost to achieve this node
-    unordered_map<int, int> visited;
+    //int: nodes been visited
+    unordered_set<int> visited;
+    //visited.insert(src);
+    
     
     //keep record of achivable nodes.
     minHeap<node> m;
     
+    node* curr = g.array[src];
+    
+    int t = 0;
+    
+    while ( t< 200){
+        visited.insert(curr->data);
+        //add all neighbors of curr node into the heap m
+        //the cost will be the sum of "cost to curr" + "curr to neighbor"
+        //these two datas are contained in the node* "neighbor_new_cost"
+        node* neighbor;
+        if (curr->next)  neighbor = curr->next;
+        else neighbor = nullptr;
+        while(neighbor){
+            if (visited.find(neighbor->data) == visited.end()){
+                node* neighbor_new_cost = new node;
+                neighbor_new_cost->data = neighbor->data;
+                neighbor_new_cost->cost = sol[curr->data] + neighbor->cost;
+                m.addNode(neighbor_new_cost);
+                neighbor= neighbor->next;
+                
+            }
+            else neighbor= neighbor->next;
+        }
+        
+        //extract the min one, if we have visited it, ignore and delete from the heap
+        while (visited.find(m.getMin()->data) != visited.end())  m.deleteMin();
+        
+        //extract the min one, it is the local greedy choice of the next step
+        
+        if (!m.empty()){
+        
+        node* next_to_go = m.getMin();
+        sol[next_to_go->data] = next_to_go->cost;
+        curr = g.array[next_to_go->data];
+        t += 1;
+        }
+        
+        //else  curr = nullptr;
+    }
         
     
     
     
-    return sol[200];
+    return visited.size();
     
     
     
@@ -79,7 +120,7 @@ int main(){
     
     cout<< dijkstra(g,199,50);
     
-    
+   
     
     return 0;
 }
