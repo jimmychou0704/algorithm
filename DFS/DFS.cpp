8b8d8543ae9c47c83b87665d5d52146143ca806e
JimmyChou <cchou9@gmail.com>
2017-02-16 18:49:15 +0000

    DFS done

--- DFS/DFS.cpp
+++ DFS/DFS.cpp
@@ -9,6 +9,7 @@ class graph{
   
         int V;      //number of nodes
         vector<vector<int>> adj;    //list of adjency edges 
+        void DFS_curr(int v, vector<bool>& visited);  //a utility functoin used by DFS
   public:
         //constructor
         graph(int num_of_node):V(num_of_node), adj(num_of_node+1){};
@@ -18,6 +19,9 @@ class graph{
         
         //print adjency edges of node v
         void printEdge(int v);
+        
+        //DFS algorithm
+        void DFS(int v);
 };
 
 void graph::addEdge(int v, int w){
@@ -28,11 +32,31 @@ void graph::printEdge(int v){
     for (auto k: adj[v]) cout<< k<< endl;
 }
 
+void graph::DFS_curr(int v, vector<bool>& visited){
+    visited[v] = true;
+    cout<< v<< " ";
+    for (auto k: adj[v]) {
+        if (!visited[k])  DFS_curr(k, visited);
+    }
+}
+
+
+void graph::DFS(int v){
+            vector<bool> visited(V, false);
+            DFS_curr(v, visited);
+            
+        }
+
 int main(){
     
-    graph g(5);
-    g.addEdge(1,3);
-    g.addEdge(1, 5);
-    g.printEdge(1);
+    graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
+    
+    g.DFS(2);
     return 0;
 }
